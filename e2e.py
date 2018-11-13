369d4c297701a67fa22c87cf3afe74d96e6c28eb
TakesxiSximada <sximada@gmail.com>
2018-11-13 15:49:19 +0900

    fix

--- e2e.py
+++ e2e.py
@@ -3,4 +3,4 @@ from selenium import webdriver
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 driver = webdriver.Chrome(chrome_options=options)
-driver.get_page('http://127.0.0.1:8000')
+driver.get('http://127.0.0.1:8000')

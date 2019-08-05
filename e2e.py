c7c0ef73e1f2aff8d872cac8175055b59de37333
rakesh635 <rakesh635@gmail.com>
2019-08-05 14:39:56 +0530

    Update e2e.py

--- e2e.py
+++ e2e.py
@@ -3,4 +3,4 @@ from selenium import webdriver
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 driver = webdriver.Chrome(chrome_options=options)
-driver.get('http://127.0.0.1:8000')
+driver.get('http://rakesh635:321661be-ee97-4df8-97c3-c05a48f84e54@ondemand.eu-central-1.saucelabs.com')
